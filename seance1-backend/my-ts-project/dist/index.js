39adb7b9920d2ed812c5712f7440328518b902a5
Ahmed Gafsi <ahmedgafsi2.0@gmail.com>
2026-03-12 04:48:43 +0100

    First session Intro to backend&TypeScript

---
+++ seance1-backend/my-ts-project/dist/index.js
@@ -0,0 +1,63 @@
+"use strict";
+// const message : string = "Hello , TypeScript";
+// let  user_name : string = "Ahmed Gafsi";
+// let count : number = 50;
+// let isMarried : boolean = false; 
+// console.log("Message recu :" ,message);
+// console.log("Count : ",count )
+// console.log("User Name : ",user_name )
+// console.log("Is User Married : ",isMarried )
+let user_name = 'Alice';
+let age = 25;
+let isActive = true;
+let data = null;
+let value = undefined;
+// to define an array of numbers we can use two syntaxes :
+let student_grades = [20, 8, 15, 13];
+// OR
+let score = [10, 20, 30, 40];
+let user_obj = {
+    name: "Ahmed Gafsi",
+    email: "ahmed@gmail.com",
+    password: 12345678
+};
+let sahar_grades = ["Sahar", "Jleli", 14];
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log("result : ", addNumbers(10, 20));
+console.log("result : ", addNumbers(100, 56));
+console.log(user_obj.email);
+let user_direction = "Gharb";
+let user_id = 123;
+user_id = 123;
+console.log("user id : ", user_id);
+let student2 = {
+    name: "Eya",
+    lastName: "Bouzidi",
+    age: 22
+};
+console.log(student2);
+let user1 = {
+    name: "Ahmed Gafsi",
+    email: "ahmed@gmail.com",
+    createdAt: new Date(),
+    id: 1,
+};
+let admin1 = {
+    name: "Eya Bouzidi",
+    email: "eya@gmail.com",
+    createdAt: new Date(),
+    role: "Admin",
+    id: 2,
+    permissions: ['read', 'write', 'delete']
+};
+user1.name = "Mortadha Maamri";
+// user1.createdAt = new Date()
+console.log(user1);
+console.log(admin1);
+function getFirstItem(arr) {
+    return arr[0];
+}
+console.log("first item is", getFirstItem([10, 20, 30]));
+console.log("first item is", getFirstItem(["ali", "mohamed", "Eya"]));
---
+++ seance1-backend/my-ts-project/src/index.ts
@@ -0,0 +1,123 @@
+// const message : string = "Hello , TypeScript";
+// let  user_name : string = "Ahmed Gafsi";
+// let count : number = 50;
+// let isMarried : boolean = false; 
+
+
+
+// console.log("Message recu :" ,message);
+// console.log("Count : ",count )
+// console.log("User Name : ",user_name )
+// console.log("Is User Married : ",isMarried )
+
+
+let user_name: string = 'Alice';
+let age: number = 25;
+let isActive: boolean = true;
+let data: null = null;
+let value: undefined = undefined;
+
+
+// to define an array of numbers we can use two syntaxes :
+let student_grades : number[] = [20,8,15,13]
+// OR
+let score : Array<number> = [10,20,30,40]
+
+let user_obj : {name:string,email:string,password:string | number} = {
+    name : "Ahmed Gafsi",
+    email : "ahmed@gmail.com",
+    password :12345678
+}
+
+
+let sahar_grades : [string,string,number] = ["Sahar","Jleli", 14]
+
+
+
+function addNumbers(a:number,b:number) :number {
+    return a+b;
+}
+
+
+console.log("result : " ,addNumbers(10,20))
+console.log("result : " ,addNumbers(100,56))
+
+console.log(user_obj.email)
+// console.log("result : " ,addNumbers("10",20)) 🔴
+// console.log("result : " ,addNumbers(5,undefined)) 🔴
+
+
+type Direction = "Chamél" | "Janoub" | "Char9" | "Gharb"
+let user_direction : Direction = "Gharb"
+
+let user_id : number | string = 123;
+user_id = 123
+console.log("user id : ",user_id)
+
+
+let student2 : unknown = {
+    name :"Eya",
+    lastName :"Bouzidi",
+    age :22
+}
+
+console.log(student2)
+
+
+interface User {
+    id : number;
+    name : string;
+    email : string;
+    age? : number;
+    readonly createdAt : Date;
+}
+
+
+type Role = "Admin" | "User" | "Manager"
+interface Admin extends User {
+    role : Role,
+    permissions : Array<string>,
+}
+
+
+
+
+
+
+let user1 : User = {
+    name : "Ahmed Gafsi",
+    email : "ahmed@gmail.com",
+    createdAt : new Date(),
+    id : 1,
+    }
+
+
+let admin1 : Admin = {
+    name : "Eya Bouzidi",
+    email : "eya@gmail.com",
+    createdAt : new Date(),
+    role :"Admin",
+    id : 2,
+    permissions : ['read','write','delete']
+}
+
+user1.name = "Mortadha Maamri"
+// user1.createdAt = new Date()
+
+console.log(user1)
+console.log(admin1)
+
+
+
+
+function getFirstItem<T>(arr:Array<T>) : T { 
+    return arr[0]
+}
+
+
+console.log("first item is" ,getFirstItem<number>([10,20,30]))
+
+console.log("first item is" ,getFirstItem<string>(["ali","mohamed","Eya"]))
+
+
+
